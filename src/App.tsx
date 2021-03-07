7cd6ff07763930f644b7613cb4608227ea15b546
FredrikEglese <fredrik.eglese@gmail.com>
2021-03-07 22:30:43 +0000

    Fixed constant re-fetching and styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,44 @@
-import React from 'react';
-import PokemonCard from './components/pokemon/pokemon';
+// npm 
+import React, { useState } from 'react';
+
+// Components
+import PokemonCard from './components/pokemonCard';
+
+// Style
 import './style/App.scss';
 
 function App() {
+  const [searchTerm, setSearchTerm] = useState("");
+  const [pokemonId, setPokemonId] = useState(0);
+
+  const handleSearchChange = (e: any) => {
+    console.log('change');
+    setSearchTerm(e.target.value);
+    if (isNumber(e.target.value)){
+      setPokemonId(e.target.value);
+    }
+  }
+
+  const isNumber = (inp: any) => {
+    return !isNaN(parseFloat(inp)) && !isNaN(inp - 0)
+  }
+
   return (
     <div className="App">
-     <PokemonCard id={5} />
+     <PokemonCard
+      id={pokemonId}
+     />
+
+    <input
+      type="text"
+      className="search-input"
+      placeholder="Pokemon ID"
+      value={searchTerm}
+      onChange={handleSearchChange}
+    />
+
     </div>
   );
 }
-
 
 export default App;
